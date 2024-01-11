471603ec37543146c994160e3d28d0f8cde03c20
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-01-11 10:27:03 +0100

    User Providers

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,9 +5,11 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -19,6 +21,8 @@ class User
 
     #[ORM\Column(length: 255)]
     private ?string $password = null;
+
+    private ?string $plainPassword = null;
 
     #[ORM\Column(type: Types::SIMPLE_ARRAY)]
     private array $roles = [];
@@ -55,6 +59,18 @@ class User
         return $this;
     }
 
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(?string $plainPassword): User
+    {
+        $this->plainPassword = $plainPassword;
+        
+        return $this;
+    }
+
     public function getRoles(): array
     {
         return $this->roles;
@@ -77,5 +93,15 @@ class User
         $this->preferredChannel = $preferredChannel;
 
         return $this;
+    }
+
+    public function eraseCredentials()
+    {
+        $this->plainPassword = null;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
     }
 }
